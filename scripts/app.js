7b166cc2bade500cf7833a471699b123ab01aac6
WebCONuz <muxammadi0799@gmail.com>
2023-03-12 16:09:02 +0500

    First commit, Responsive Homepage

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".menu-bar");
+const menu = document.querySelector(".wrapper");
+
+menuBtn.addEventListener("click", function () {
+  if (!menu.classList.contains("active")) {
+    menu.classList.add("active");
+    this.innerHTML =
+      '<img src="./images/icons/close.png" alt="menu-icon" style="width:20px;" />';
+  } else {
+    menu.classList.remove("active");
+    this.innerHTML =
+      '<img src="./images/icons/menu.png" alt="close-icon" style="width:20px;" />';
+  }
+});
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("scrolled", window.scrollY > 0);
+});
+
+// Blog Carousel -----------------------------------
+
+var owl1 = $(".blog__cards");
+owl1.owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  margin: 25,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+  },
+});
+
+// Team Carousel -----------------------------------
+
+var owl2 = $(".team__cards");
+owl2.owlCarousel({
+  loop: true,
+  nav: true,
+  dots: false,
+  margin: 20,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    576: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    1024: {
+      items: 4,
+    },
+  },
+});
